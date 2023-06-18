ab4d49c095b9757157796856d99351e67c4d5767
Anton Harankou <harankou.anton@gmail.com>
2023-06-18 18:47:21 +0300

    Update upload function (publish layer on geoserver)

--- profiles/upload_data.py
+++ profiles/upload_data.py
@@ -3,9 +3,11 @@ from profiles.models import Layer, LayerAccess
 from sqlalchemy import create_engine
 import geopandas as gpd
 import uuid
+from geo.Geoserver import Geoserver
 
 
 def uploading_process(file_name, table_name, owner_layer):
+    # Create connection to database
     user = settings.DATABASES['default']['USER']
     password = settings.DATABASES['default']['PASSWORD']
     host = settings.DATABASES['default']['HOST']
@@ -14,9 +16,16 @@ def uploading_process(file_name, table_name, owner_layer):
     conn = f"postgresql://{user}:{password}@{host}:{port}/{name}"
     engine = create_engine(conn)
 
+    # Upload data to database's tables
     uuid_table = str(uuid.uuid4())
     gdf = gpd.read_file(file_name)
     gdf.to_postgis(name=uuid_table, con=engine, schema='geodata')
     Layer.objects.create(layer_id=uuid_table, table_name=table_name)
     LayerAccess.objects.create(user_id_id=owner_layer, layer_id_id=uuid_table, access_code_id=0)
+
+    # Publish layer on geoserver
+    geo = Geoserver(service_url=f'http://{settings.GEOSERVER["HOST"]}:{settings.GEOSERVER["PORT"]}/geoserver',
+                    username=settings.GEOSERVER["LOGIN_GEOSERVER"],
+                    password=settings.GEOSERVER["PASSWORD_GEOSERVER"])
+    geo.publish_featurestore(store_name='geo_data', pg_table=uuid_table, workspace='layers')
     return
